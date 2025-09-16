b2afb20eba83b57631bcfab999cb3ddcb2e4cf9b
Sidiqzharly <165438324+Sidiqzharly@users.noreply.github.com>
2025-09-16 07:11:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function showPage(pageId) {
+  // Hide all pages
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => page.classList.remove('active'));
+
+  // Show selected page
+  document.getElementById(pageId).classList.add('active');
+}
+
+// Show home by default
+window.onload = () => showPage('home');
